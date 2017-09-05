8177d6c9ab559e52119c0a64c507b3359401b788
lingdian <lingdian@meili-inc.com>
2017-09-05 23:41:15 +0800

    first commit

--- 06UIWindow/AppDelegate.m
+++ 06UIWindow/AppDelegate.m
@@ -14,9 +14,49 @@
 
 @implementation AppDelegate
 
-
+//框架初始化 成功之后 调用此函数，此函数用来初始化整个程序框架结构的，对于ios程序员来说 是开发的入口
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+   
+    //创建一个uiwindows对象，整个应用只有一个UIWindow对象  在程序级别上 表示窗口屏幕
+    //UIWindow也是继承UIView 的。是一个特殊的UIView。
+    //UIScreen 是一个硬件信息的工具类 里面 有硬件的信息 比如设备的宽高等
+    //mainScreen 获取主屏幕的设备信息
+    //bounds 表示屏幕的宽高值
+    self.window=[[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    //创建一个视图控制器 作为UIWindow的根视图  相当于把视图控制器的view 添加到了 UIWindow
+    self.window.rootViewController=[[UIViewController alloc]init];//
+    //设置背景颜色
+    [self.window setBackgroundColor:[UIColor blueColor]];
+    
+    UIView* view=[[UIView alloc]initWithFrame:CGRectMake(100, 100, 150, 150)];
+    [view setBackgroundColor:[UIColor orangeColor]];
+
+    UIView* parentView=[[UIView alloc]initWithFrame:CGRectMake(0, 0, 240, 360)];
+    [parentView setBackgroundColor:[UIColor greenColor]];
+    
+    //view添加奥parentview 中作为他的一个子视图
+    //那么view子视图 的坐标是参照parentview 父视图的坐标系
+    //如果父视图的位置发生了变化  那么所有的子视图也会跟着变化
+    [parentView addSubview:view];
+    
+    //添加一个parentView到windows上  这相当于第二个子view 第一个是UIViewController的view
+    [self.window addSubview:parentView];
+    
+    
+    //每一个view 都有一个 window属性 而且window都是同一个window 都是这个self.window
+    //addview 的时候 会把window 赋值给我每个view的window
+    NSLog(@"view window%@",view.window);
+    
+    NSLog(@"parentView window%@",parentView.window);
+    
+    NSLog(@"self window%@",self.window);
+    
+    NSLog(@"self window%@",self.window.rootViewController.view.window);
+    
+    
+    //使UIWidows 有效果 并显示在屏幕上
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
